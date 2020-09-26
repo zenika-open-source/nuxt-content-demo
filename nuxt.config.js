708129a4287034742251ca3a151d94b535caab79
Yann Bertrand <5855339+yannbertrand@users.noreply.github.com>
2020-09-26 18:10:45 +0200

    :zap: Ajoute la config target static pour réduire la durée de build

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  // Depuis nuxt@2.14, on peut ajouter :
+  target: 'static',
+  // Qui permet de mettre en cache les fichiers générés
+  // Pour réduire le temps de compilation !
+  // Voir https://nuxtjs.org/blog/nuxt-static-improvements
   components: true,
   modules: [
     '@nuxt/content'
